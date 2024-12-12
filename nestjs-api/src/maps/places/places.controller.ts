03d69953787e3d04a009d5d16a818d5ff509a63f
unknown <paesguigo@gmail.com>
2024-12-12 10:13:15 -0300

    Adição da pasta

---
+++ nestjs-api/src/maps/places/places.controller.ts
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+import { Controller, Query, Get } from '@nestjs/common';
+import { PlacesService } from './places.service';
+
+@Controller('places')
+export class PlacesController {
+  constructor(private readonly placesService: PlacesService) {}
+  @Get()
+  findPlaces(@Query('text') text: string) {
+    return this.placesService.findPlaces(text);
+  }
+}
